7053a952d2d144274d45a354b2fd92d879ae867a
Benanna2019 <bass41992ben@gmail.com>
2020-09-25 17:05:24 -0400

    Made the delete-like route for front end app

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-require('dotenv').config();
-const sql = require('mysql2/promise');
-const express = require('express');
-const bcrypt = require('bcrypt');
+require("dotenv").config();
+const sql = require("mysql2/promise");
+const express = require("express");
+const bcrypt = require("bcrypt");
 const PORT = 4000;
-const jwt = require('jsonwebtoken');
-const cors = require('cors');
-const { request, response } = require('express');
+const jwt = require("jsonwebtoken");
+const cors = require("cors");
+const { request, response } = require("express");
 
 const app = express();
 
@@ -18,11 +18,11 @@ const pool = sql.createPool({
   password: process.env.password,
 });
 
-app.post('/get-liked-pets', authorizeUser, async (request, response) => {
+app.post("/get-liked-pets", authorizeUser, async (request, response) => {
   try {
     const username = request.decodedToken.username;
     const conn = await pool.getConnection();
-    conn.query('USE petsAreUs');
+    conn.query("USE petsAreUs");
     const likedPets = await conn.execute(`
     SELECT * FROM likes 
 	    JOIN pets ON pets.id = likes.petid 
@@ -36,17 +36,17 @@ app.post('/get-liked-pets', authorizeUser, async (request, response) => {
   }
 });
 
-app.post('/like-pet', authorizeUser, async (request, response) => {
+app.post("/like-pet", authorizeUser, async (request, response) => {
   try {
     if (!request.body.petid)
-      return response.status(401).send({ message: 'Missing required field' });
+      return response.status(401).send({ message: "Missing required field" });
     const username = request.decodedToken.username;
     const petid = request.body.petid;
     console.log(username, petid);
     const conn = await pool.getConnection();
-    conn.query('USE petsAreUs');
+    conn.query("USE petsAreUs");
     const resp = await conn.execute(
-      `INSERT INTO likes (petid, user) VALUES (${petid},'${username}')`,
+      `INSERT INTO likes (petid, user) VALUES (${petid},'${username}')`
     );
     conn.release();
     response.status(200).send(resp);
@@ -56,14 +56,34 @@ app.post('/like-pet', authorizeUser, async (request, response) => {
   }
 });
 
-app.post('/get-all-pets', authorizeUser, async (request, response) => {
+app.post("/delete-like", authorizeUser, async (request, response) => {
+  try {
+    if (!request.body.petid)
+      return response.status(401).send({ message: "Missing required field" });
+    const username = request.decodedToken.username;
+    const petid = request.body.petid;
+    console.log(username, petid);
+    const conn = await pool.getConnection();
+    conn.query("USE petsAreUs");
+    const resp = await conn.execute(
+      `DELETE FROM likes WHERE petid = ${petid} AND user = '${username}'`
+    );
+    conn.release();
+    response.status(200).send(resp);
+  } catch (error) {
+    console.log(error);
+    response.status(500).send({ message: error });
+  }
+});
+
+app.post("/get-all-pets", authorizeUser, async (request, response) => {
   try {
     const username = request.decodedToken.username;
     const conn = await pool.getConnection();
-    conn.query('USE petsAreUs');
+    conn.query("USE petsAreUs");
     const otherPets = await conn.execute(
       `SELECT * FROM pets WHERE username!=?`,
-      [username],
+      [username]
     );
     conn.release();
     response.status(200).send(otherPets[0]);
@@ -73,12 +93,12 @@ app.post('/get-all-pets', authorizeUser, async (request, response) => {
   }
 });
 
-app.post('/get-your-pets', authorizeUser, async (request, response) => {
-  console.log('get your pets hit');
+app.post("/get-your-pets", authorizeUser, async (request, response) => {
+  console.log("get your pets hit");
   try {
     const username = request.decodedToken.username;
     const conn = await pool.getConnection();
-    conn.query('USE petsAreUs');
+    conn.query("USE petsAreUs");
     const yourPets = await conn.execute(`SELECT * FROM pets WHERE username=?`, [
       username,
     ]);
@@ -90,36 +110,36 @@ app.post('/get-your-pets', authorizeUser, async (request, response) => {
   }
 });
 
-app.post('/login', async (request, response) => {
+app.post("/login", async (request, response) => {
   try {
     if (!request.body.username || !request.body.password)
       return response
         .status(401)
-        .send({ message: 'Missing username or password' });
+        .send({ message: "Missing username or password" });
 
     const username = request.body.username;
     const password = request.body.password;
     const conn = await pool.getConnection();
     const queryResponse = await conn.execute(
       `SELECT * FROM petsAreUs.user WHERE username = ?`,
-      [username],
+      [username]
     );
     const fetchedUser = queryResponse[0][0];
     if (!fetchedUser)
-      response.status(401).send({ message: 'User does not exist' });
+      response.status(401).send({ message: "User does not exist" });
     else {
       if (await bcrypt.compare(password, fetchedUser.password)) {
         const username = fetchedUser.username;
 
         const jwtToken = jwt.sign(
           { username: username },
-          process.env.ACCESS_TOKEN_KEY,
+          process.env.ACCESS_TOKEN_KEY
         );
         response
           .status(200)
-          .send({ message: 'successfully authenticated', jwt: jwtToken });
+          .send({ message: "successfully authenticated", jwt: jwtToken });
       } else {
-        response.status(401).send({ message: 'incorrect password' });
+        response.status(401).send({ message: "incorrect password" });
       }
     }
 
@@ -131,12 +151,12 @@ app.post('/login', async (request, response) => {
   }
 });
 
-app.post('/user', async (request, response) => {
+app.post("/user", async (request, response) => {
   try {
     if (!request.body.username || !request.body.password)
       return response
         .status(401)
-        .send({ message: 'Missing username or password' });
+        .send({ message: "Missing username or password" });
 
     const username = request.body.username;
     const password = request.body.password;
@@ -145,7 +165,7 @@ app.post('/user', async (request, response) => {
     const conn = await pool.getConnection();
     const queryResponse = await conn.execute(
       `INSERT INTO petsAreUs.user (username, password) VALUES (?,?)`,
-      [username, hashedPassword],
+      [username, hashedPassword]
     );
     conn.release();
     response.status(201).send(queryResponse);
@@ -156,10 +176,10 @@ app.post('/user', async (request, response) => {
 });
 
 function authorizeUser(request, response, next) {
-  console.log('auth hit');
+  console.log("auth hit");
   const token = request.body.jwt;
   if (token == null) {
-    console.log(token, 'token is null');
+    console.log(token, "token is null");
     return response.status(401).send();
   }
   jwt.verify(token, process.env.ACCESS_TOKEN_KEY, (err, decodedToken) => {
